060812287d4c193fff2ffadef34c42ed9703c295
Amanyadav1234-coder <amanyadav97953@gmail.com>
2025-01-21 22:51:15 +0530

    Index page

---
+++ index.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning Management System</title>
+    <link rel="shortcut icon" href="img/elearning-logo.webp" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- outer start -->
+     <div class="container-fluid">
+        <div class="row sticky-top bg-light">
+            <div class="col-sm-12">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+                    <div class="container-fluid">
+                      <a class="navbar-brand text-sky ps-4" href="index.html"><i class="fa-solid fa-book fs-2 me-2 text-sky"></i>eLEARNING</a>
+                      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                      </button>
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                          <li class="nav-item">
+                            <a class="nav-link active px-4 py-3" aria-current="page" href="index.php">Home</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link active px-4 py-3" aria-current="page" href="about.php">About Us</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link active px-4 py-3" aria-current="page" href="login.php">Login</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link active px-4 py-3" aria-current="page" href="register.php">Register</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link active ps-4 py-3" aria-current="page" href="contact.php">contact us</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link active ps-4" aria-current="page" href="#">
+                              <button type="button" class="btn btn-info text-light">Join Now <i class="fa-solid fa-arrow-right text-light"></i></button>
+                            </a>
+                          </li>
+                         
+                          
+                      </div>
+                    </div>
+                  </nav>
+            </div>
+           
+        </div>
+        <div class="row ">
+          <div class="col-sm-12 p-0">
+            <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
+              <div class="carousel-inner">
+                <div class="carousel-item active back-img" data-bs-interval="10000">
+                 <div class="row">
+                  <div class="col-sm-10 mx-auto m " data-aos="fade-down"
+                  data-aos-easing="linear"
+                  data-aos-duration="1500">
+                    <h4 class="text-sky">BEST ONLINE COURSES</h4>
+                    <h1 class="text-light fw-bold">Get Educated Online</h1>
+                    <h1 class="text-light fw-bold">From Your Home</h1>
+                    <h6 class="text-light fw-bold">Vero elitr justo clita lorem.  Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum <br>et diam justo clita et kasd rebum sea sanctus eirmod elitr.</h6>
+                    <button class="btn btn-info rounded-0 px-4 py-2 mt-3">Read More</button>
+                    <button class="btn btn-light rounded-0 px-4 py-2 mt-3 ms-2">Join Now</button>
+
+                  </div>
+                 </div>
+                </div>
+                <div class="carousel-item back-img1  " data-bs-interval="2000">
+                  <div class="col-sm-10 mx-auto m">
+                    <h4 class="text-sky">BEST ONLINE COURSES</h4>
+                    <h1 class="text-light fw-bold">Get Educated Online</h1>
+                    <h1 class="text-light fw-bold">From Your Home</h1>
+                    <h6 class="text-light fw-bold">Vero elitr justo clita lorem.Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum<br> et diam justo clita et kasd rebum sea sanctus eirmod elitr.</h6>
+                    <button class="btn btn-info rounded-0 px-4 py-2 mt-3">Read More</button>
+                    <button class="btn btn-light rounded-0 px-4 py-2 mt-3 ms-2">Join Now</button>
+                  </div>
+                </div>
+                <div class="carousel-item back-img2">
+                  <div class="col-sm-10 mx-auto m">
+                    <h4 class="text-sky">BEST ONLINE COURSES</h4>
+                    <h1 class="text-light fw-bold">Get Educated Online</h1>
+                    <h1 class="text-light fw-bold">From Your Home</h1>
+                    <h6 class="text-light fw-bold ">Vero elitr justo clita lorem.Ipsum dolor at sed stet sit diam no. Kasd rebum ipsum<br> et diam justo clita et kasd rebum sea sanctus eirmod elitr.</h6>
+                    <button class="btn btn-info rounded-0 px-4 py-2 mt-3">Read More</button>
+                    <button class="btn btn-light rounded-0 px-4 py-2 mt-3 ms-2">Join Now</button>
+                  </div>
+                </div>
+              </div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+          </div>
+        </div>
+       <div class="row my-5">
+        <div class="col-sm-11 mx-auto">
+          <div class="row row-cols-2 row-cols-md-4 g-4">
+            <div class="col  ">
+              <div class="card p-3 text-center border-0 shadow-lg shadow-dark text-sky ca" data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-duration="1500">
+                <i class="fa-solid fa-graduation-cap fs-1 "></i>
+                <h5 class=" mt-3">Skilled Instructors
+                </h5>
+                <p class=" justify mt-2">Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+              </div>
+            </div>
+          
+            <div class="col">
+              <div class="card p-3 text-center border-0 shadow-lg  text-sky shadow-dark ca" data-aos="fade-up"
+              data-aos-easing="linear"
+              data-aos-duration="1500">
+                <i class="fa-solid fa-earth-oceania fs-1"></i>
+                <h5 class=" mt-3">Online Classes
+
+                </h5>
+                <p class=" justify mt-2">Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+              </div>
+            </div>
+          
+            <div class="col">
+              <div class="card p-3 text-center border-0 shadow-lg shadow-dark ca text-sky" data-aos="fade-down"
+              data-aos-easing="linear"
+              data-aos-duration="1500">
+                <i class="fa-solid fa-house fs-1"></i>
+                <h5 class=" mt-3">Home Projects
+
+                </h5>
+                <p class=" justify mt-2">Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+              </div>
+            </div>
+          
+            <div class="col">
+              <div class="card p-3 text-center border-0 shadow-lg shadow-dark ca text-sky" data-aos="fade-up"
+              data-aos-easing="linear"
+              data-aos-duration="1500">
+                <i class="fa-solid fa-book-open fs-1"></i>
+                <h5 class=" mt-3">Book Library
+
+                </h5>
+                <p class=" justify mt-2">Diam elitr kasd sed at elitr sed ipsum justo dolor sed clita amet diam</p>
+              </div>
+            </div>
+          
+          </div>
+        </div>
+       </div>
+       <div class="row my-5">
+        <div class="col-sm-11 mx-auto text-sky ">
+          <div class="card mb-3 border-0">
+            <div class="row g-0 p-2">
+              <div class="col-md-5 me-5">
+                <div class="card-body ca rounded-4 p-5"data-aos="fade-right"
+                data-aos-duration="3000" >
+                  <h1 class="card-title " id="lh">About Us</h1>
+                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                </div>
+              </div>
+              <div class="col-md-6" data-aos="fade-up"
+              data-aos-duration="3000">
+                <img src="img/about-img.png" class="img-fluid rounded-start" alt="...">
+              </div>  
+            </div>
+          </div>
+        </div>
+       </div>
+       <div class="row back-img3 text-light">
+        <div class="col-sm-11 mx-auto">
+          <div class="row mt-5">
+            <div class="col-sm-12">
+              <p class="text-center"><img src="img/lms-logo.png" alt="" data-aos="fade-down"
+                data-aos-duration="3000"></p>
+            </div>
+            <div class="row">
+              <div class="col-sm-6 mx-auto" data-aos="fade-up"
+              data-aos-duration="3000">
+                <h3>Powerful Learning Management System WP Theme</h3>
+              </div>
+              <div class="row">
+                <div class="col-sm-10 mx-auto" data-aos="fade-up"
+                data-aos-duration="3000">
+                  <h5 class="justify"> Awesome features for creating online courses, teacher profile, extended user profiles, lesson management, quiz system, video hosting, ranking system, question system, attachments, course progress, etc.,</h5>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-sm-10 mx-auto">
+                  <img src="img/lms-devices.png" alt="" class="w-100" data-aos="flip-left"
+                  data-aos-easing="ease-out-cubic"
+                  data-aos-duration="2000">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+       </div>
+       <div class="row pb-5" style="background-color: #FDF6EA; padding-top: 200px;">
+        <div class="col-sm-11 mx-auto">
+          <div class="row row-cols-2 row-cols-md-4 g-4">
+            <div class="col">
+              <div class="card h-100 border-0 bg-org hover" data-aos="fade-down"
+     data-aos-duration="3000">
+                <p class="text-center px-5"><img src="img/c1.png" class="card-img-top w-50 rounded-circle" alt="..." style="height: 100px;"></p>
+                <h5 class="card-title text-center">One Click Demo Import</h5>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100 border-0 bg-org hover" data-aos="fade-up"
+     data-aos-duration="3000">
+                <p class="text-center px-5"><img src="img/{121AFF88-1256-41B2-B37D-5B72DF72D20D}.png" class="card-img-top w-50 rounded-circle" alt="..." style="height: 100px;"></p>
+                <h5 class="card-title text-center">
+                  No need of Code Skills</h5>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100 border-0 bg-org hover" data-aos="fade-down"
+     data-aos-duration="3000">
+                <p class="text-center px-5"><img src="img/{198C7EB3-0BBB-4103-8ACA-07D957CDB494}.png" class="card-img-top w-50 rounded-circle" alt="..." style="height: 100px;"></p>
+                <h5 class="card-title text-center">Responsive for All Devices</h5>
+              </div>
+            </div>
+            <div class="col">
+              <div class="card h-100 border-0 bg-org hover" data-aos="fade-up"
+     data-aos-duration="3000">
+                <p class="text-center px-5"><img src="img/{AE0CFC38-8F8F-4486-9805-FD8336D8D446}.png" class="card-img-top w-50 rounded-circle" alt="..." style="height: 100px;"></p>
+                <h5 class="card-title text-center">NexGen Documentation</h5>
+              </div>
+            </div>
+         
+          </div>
+        </div>
+       </div>
+       <div class="row mt-2 ">
+        <div class="col-sm-9 mx-auto">
+         <div class="row">
+          <div class="col-sm-10 my-4">
+            <h6>Copyright © 2025 LMS Theme All Rights Reserved
+              <b>Aman Yadav</b></h6>
+          </div>
+          <div class="col-sm-2 my-4">
+            <span ><i class="fa-brands fa-twitter fs-5 px-2 h"></i></span>
+            <span><i class="fa-brands fa-facebook fs-5 px-2 h"></i></span>
+            <span><i class="fa-brands fa-youtube fs-5 px-2 h"></i></span>
+            <span><i class="fa-brands fa-instagram fs-5 px-2 h"></i></span>
+          </div>
+         </div>
+        </div>
+       </div>
+     </div>
+    <!-- outer end -->
+   
+   <script src="js/bootstrap.bundle.js"></script> 
+   <script>
+
+    if (document.getElementById('slider1') != null) {
+      var slider1 = new Splide('#slider1', {
+        type: 'loop',
+        perPage: 5,
+        rewindByDrag: true,
+        focus: 0,
+        autoplay: true,
+        speed: 1000,
+
+        breakpoints: {
+          991: {
+            perPage: 2
+          },
+          561: {
+            perPage: 1
+          },
+        }
+      });
+
+      slider1.mount();
+    }
+
+  </script>
+<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
+</body>
+</html>
